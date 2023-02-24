023070a35defede56a8d8d3ee29e2dcde321a7b9
JDJG Inc. Official <ghostzap123@gmail.com>
2023-02-24 00:06:52 -0500

    adds blacklist table and blacklist type.

--- table.sql
+++ table.sql
@@ -6,3 +6,12 @@ CREATE TABLE IF NOT EXISTS SICRONI_GLOBAL_CHAT(
    UNIQUE ("server_id", "channel_id"),
    PRIMARY KEY ("server_id", "chat_type")
 )
+
+CREATE TABLE IF NOT EXISTS SINCRONI_BLACKLIST(
+   server_id BIGINT NOT NULL,
+   entity_id BIGINT NOT NULL,
+   pub BOOLEAN DEFAULT FALSE,
+   dev BOOLEAN DEFAULT FALSE,
+   private BOOLEAN DEFAULT FALSE,
+   blacklist_type SMALLINT DEFAULT 0 
+)
